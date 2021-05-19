dcb19a664a8b597e1a98fbf05c4b7792371963cb
Haikyuu77 <62696931+Haikyuu77@users.noreply.github.com>
2021-05-19 14:59:13 +0530

    Delete searchinsertposition.cpp

--- searchinsertposition.cpp
+++
@@ -1,6 +0,0 @@
- int searchInsert(vector<int>& nums, int target) 
-    {
-        auto it = lower_bound(nums.begin(),nums.end(),target);
-        int index = distance(nums.begin(),it);
-        return index;
-    }
