79281e638a6557b8fc4ecd672c2b729ef891ea4e
Haikyuu77 <62696931+Haikyuu77@users.noreply.github.com>
2021-05-19 15:02:31 +0530

    Update searchinsertposition.cpp

--- leetcode/searchinsertposition.cpp
+++ leetcode/searchinsertposition.cpp
@@ -1,6 +1,9 @@
- int searchInsert(vector<int>& nums, int target) 
+class solution { 
+public:
+int searchInsert(vector<int>& nums, int target) 
     {
         auto it = lower_bound(nums.begin(),nums.end(),target);
         int index = distance(nums.begin(),it);
         return index;
     }
+};
