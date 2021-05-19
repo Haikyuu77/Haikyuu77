1dd5cc7b10236f5338c9dc197e139c405360a954
Haikyuu77 <62696931+Haikyuu77@users.noreply.github.com>
2021-05-19 23:59:13 +0530

    Create duplicateremovalsortedarray.cpp

---
+++ leetcode/duplicateremovalsortedarray.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) 
+    {
+        if(nums.empty()) 
+            return 0;
+        int i = 0, j = 0;
+        while(++j<nums.size())
+        {
+            if(nums[i]!=nums[j])
+            {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i+1;
+    }    
+
+};
